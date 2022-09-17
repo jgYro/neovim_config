6b343eae063b318a5747def4a0c7a351fd2b42ba
Jericho C. Gregory <yro@Jerichos-MacBook-Air.local>
2022-09-17 18:19:32 -0400

    Improved Toggleterm integration with SO

---
+++ lua/term.lua
@@ -0,0 +1,2 @@
+-- Added so the SO integration runs a bit better so text can be selected in the pop up
+require("toggleterm").setup{}
--- lua/witch.lua
+++ lua/witch.lua
@@ -62,7 +62,7 @@ wk.register({
         name = "Search",
         c = { "<Cmd>lua require('Utils.cht').so_input()<cr>", "cht.sh" },
         s = { "<Cmd>lua require('Utils.term').so()<cr>", "Stack Overflow" },
-        w = { "<Cmd>lua require('telescope').extensions.arecibo.websearch()<cr>", "Web" },
+        -- w = { "<Cmd>lua require('telescope').extensions.arecibo.websearch()<cr>", "Web" },
 
     },
      
