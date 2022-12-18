5ad6e32f1bad9bbc6b6064eb0668d95b837b8c15
Jericho C. Gregory <46950626+jgYro@users.noreply.github.com>
2022-12-18 14:18:45 -0500

    Create set.lua
    Updating and simplifying

---
+++ lua/Yro/set.lua
@@ -0,0 +1,48 @@
+-- Set line numbers
+vim.opt.nu = true
+
+-- Set relative line numbers
+vim.opt.relativenumber = true
+
+-- I like how these look
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = true
+
+-- Formatting
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- Autoindenting
+vim.opt.smartindent = true
+
+-- Disable word wrap
+vim.opt.wrap = false
+
+-- No swapfile, set up (plugin) undo tree
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+-- Disable highlighting previous search
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Better vim colors
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+-- Decrease updatetime
+vim.opt.updatetime = 50
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
