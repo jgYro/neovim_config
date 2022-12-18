2d3484c42fed5f71ade64781e57ace67e1511ddc
Jericho C. Gregory <46950626+jgYro@users.noreply.github.com>
2022-12-18 16:49:15 -0500

    Create init.lua

---
+++ lua/Yro/Plugins/Packer/init.lua
@@ -0,0 +1,28 @@
+-- Install packer
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+local is_bootstrap = false
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+    is_bootstrap = true
+    vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+    vim.cmd [[packadd packer.nvim]]
+end
+
+require('packer').startup(function(use)
+    -- Package manager
+    use 'wbthomason/packer.nvim'
+
+    use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use({
+	  'rose-pine/neovim',
+	  as = 'rose-pine',
+	  config = function()
+		  vim.cmd('colorscheme rose-pine')
+	  end
+  })
+
+end)
