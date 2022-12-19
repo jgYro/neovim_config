c98338782f1480a4df7bee81826c3eb4fbcc4cf7
Jericho C. Gregory <jericho-g@imaginehappens.com>
2022-12-18 20:16:42 -0500

    Added dap support for dart lang

--- lua/Yro/Plugins/DAP/init.lua
+++ lua/Yro/Plugins/DAP/init.lua
@@ -19,6 +19,25 @@ dap.configurations.rust = {
     },
 }
 
+dap.adapters.dart = {
+    type = "executable",
+    command = "dart",
+    -- This command was introduced upstream in https://github.com/dart-lang/sdk/commit/b68ccc9a
+    args = { "debug_adapter" }
+}
+dap.configurations.dart = {
+    {
+        type = "dart",
+        request = "launch",
+        name = "Launch Dart Program",
+        -- The nvim-dap plugin populates this variable with the filename of the current buffer
+        program = "${file}",
+        -- The nvim-dap plugin populates this variable with the editor's current working directory
+        cwd = "${workspaceFolder}",
+        args = { "--help" }, -- Note for Dart apps this is args, for Flutter apps toolArgs
+    }
+}
+
 require('dap-python').setup('/Library/Frameworks/Python.framework/Versions/3.10/bin/python3')
 
 require("nvim-dap-virtual-text").setup()
